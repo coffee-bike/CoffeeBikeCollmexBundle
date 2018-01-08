32c3b070e38b01a44c68de326ac7eecd300adfe5
Jonas Dambacher <mail@jonas-dambacher.de>
2018-01-08 09:34:49 +0100

    Removed duplicated table name

--- Entity/PriceGroup.php
+++ Entity/PriceGroup.php
@@ -5,37 +5,12 @@ namespace CoffeeBike\CollmexBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="cm_product_group")
- */
 class PriceGroup extends CollmexObject
 {
-    /**
-     * @ORM\Column(type="string")
-     */
     protected $type_identifier = "PRICE_GROUP";
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     */
     protected $company_id;
-    /**
-     * @ORM\Column(type="integer")
-     */
     protected $id;
-    /**
-     * @ORM\Column(type="string")
-     */
     protected $name;
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     */
     protected $gross_prices;
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="string")
-     */
     protected $currency;
 }
